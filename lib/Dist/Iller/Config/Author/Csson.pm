72941d55aca1b1732c07afb96aea1bec2150ca74
Erik Carlsson <info@code301.com>
2015-02-06 21:52:51 +0100

    Init

---
+++ lib/Dist/Iller/Config/Author/Csson.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Dist::Iller::Config::Author::Csson;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Iller::Config::Author::Csson;
+
+=head1 DESCRIPTION
+
+Dist::Iller::Config::Author::Csson is ...
+
+=head1 SEE ALSO
+
+=cut
