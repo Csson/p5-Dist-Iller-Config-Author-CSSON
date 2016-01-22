16dd467ff2dd420ded6c6a9464747e02c0ad6dac
Erik Carlsson <info@code301.com>
2016-01-22 15:12:49 +0000

    Added MapMetro config

---
+++ lib/Dist/Iller/Config/Author/CSSON/MapMetro.pm
@@ -0,0 +1,29 @@
+use 5.14.0;
+use warnings;
+
+package Dist::Iller::Config::Author::CSSON::MapMetro {
+
+	use Moose;
+	with 'Dist::Iller::Role::Config';
+
+	# ABSTRACT: Dist::Iller config for Map::Metro
+    # VERSION
+
+}
+
+1;
+
+=pod
+
+:splint classname Dist::Iller::Config::Author::CSSON::MapMetro
+
+=head1 SYNOPSIS
+
+    # in iller.yaml
+    +config: Author::CSSON::MapMetro
+
+=head1 DESCRIPTION
+
+Dist::Iller::Config::Author::Csson::MapMetro is a L<Dist::Iller> configuration. The plugin list is in C<share/author-csson-mapmetro.yaml>.
+
+=cut
