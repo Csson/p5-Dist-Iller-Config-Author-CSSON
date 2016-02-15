a803ba84c2d0a92606b4124108d7a6d6f8f2592f
Erik Carlsson <info@code301.com>
2016-02-15 13:37:31 +0000

    Release 0.0305
     [Requirements]
     - Lower to 5.10.0 everywhere.
    
     [Stats & Dependency Changes]
     - 3 files changed, 3 insertions(+), 3 deletions(-)
     - (run req) ~ perl 5.010001 --> 5.010000
     - (dev req) ~ Dist::Iller::Config::Author::CSSON 0.0304 --> 0.0305

--- lib/Dist/Iller/Config/Author/CSSON.pm
+++ lib/Dist/Iller/Config/Author/CSSON.pm
@@ -6,7 +6,7 @@ package Dist::Iller::Config::Author::CSSON;
 
 # ABSTRACT: Dist::Iller config
 # AUTHORITY
-our $VERSION = '0.0305';
+our $VERSION = '0.0306';
 
 use Moose;
 use namespace::autoclean;
--- lib/Dist/Iller/Config/Author/CSSON/MapMetro.pm
+++ lib/Dist/Iller/Config/Author/CSSON/MapMetro.pm
@@ -6,7 +6,7 @@ package Dist::Iller::Config::Author::CSSON::MapMetro;
 
 # ABSTRACT: Dist::Iller config for Map::Metro
 # AUTHORITY
-our $VERSION = '0.0305';
+our $VERSION = '0.0306';
 
 use Moose;
 extends 'Dist::Iller::Config::Author::CSSON';
