99ea63415affb7e9bc01d64b6751cc3999dd0fcf
Erik Carlsson <info@code301.com>
2016-02-15 13:10:52 +0000

    Release 0.0304
     [Enhancements]
     - Added plugins to config.
     - Improved (hopefully) the order of some plugins.
    
     [Stats & Dependency Changes]
     - 4 files changed, 49 insertions(+), 10 deletions(-)
     - (dev req) + Dist::Zilla::Plugin::Authority 1.009
     - (dev req) + Dist::Zilla::Plugin::ChangeStats::Dependencies::Git (any)
     - (dev req) + Dist::Zilla::Plugin::Git::CheckFor::CorrectBranch 0.013
     - (dev req) + Dist::Zilla::Plugin::Git::Commit (any)
     - (dev req) ~ Dist::Iller 0.1402 --> 0.1405
     - (dev req) ~ Dist::Iller::Config::Author::CSSON 0.0303 --> 0.0304
     - (dev req) ~ Dist::Zilla::Plugin::RewriteVersion::Transitional (any) -->
       0.007

--- lib/Dist/Iller/Config/Author/CSSON.pm
+++ lib/Dist/Iller/Config/Author/CSSON.pm
@@ -6,7 +6,7 @@ package Dist::Iller::Config::Author::CSSON;
 
 # ABSTRACT: Dist::Iller config
 # AUTHORITY
-our $VERSION = '0.0304';
+our $VERSION = '0.0305';
 
 use Moose;
 use namespace::autoclean;
