1d12db4ec3e22084526fd9c69f2d8781b2251707
Erik Carlsson <info@code301.com>
2017-03-31 19:27:31 +0000

    Bump version after 0.0314

--- lib/Dist/Iller/Config/Author/CSSON/MapMetro.pm
+++ lib/Dist/Iller/Config/Author/CSSON/MapMetro.pm
@@ -6,7 +6,7 @@ package Dist::Iller::Config::Author::CSSON::MapMetro;
 
 # ABSTRACT: Dist::Iller config for Map::Metro
 # AUTHORITY
-our $VERSION = '0.0314';
+our $VERSION = '0.0315';
 
 use Moose;
 extends 'Dist::Iller::Config::Author::CSSON';
