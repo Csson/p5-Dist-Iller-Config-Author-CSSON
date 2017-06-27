1d58924cfae71f65beaf477447e44d8542d47324
Erik Carlsson <info@code301.com>
2017-06-27 20:08:43 +0000

    Bump version and prereqs

--- lib/Dist/Iller/Config/Author/CSSON.pm
+++ lib/Dist/Iller/Config/Author/CSSON.pm
@@ -6,7 +6,7 @@ package Dist::Iller::Config::Author::CSSON;
 
 # ABSTRACT: Dist::Iller config
 # AUTHORITY
-our $VERSION = '0.0315';
+our $VERSION = '0.0316';
 
 use Moose;
 use namespace::autoclean;
